ce86f94ef151b1e7aa11e0eb24dd904097045193
Matheus Henrique Nunes <matheushnunes2005@gmail.com>
2025-07-25 16:09:15 -0300

    HashRouter para github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles/mediaQuerys.scss'
 import './styles/animations.css'
 import './styles/reset.css'
@@ -10,8 +10,8 @@ import App from './App.jsx'
 let root = document.getElementById('root');
 createRoot(root).render(
     <StrictMode>
-        <BrowserRouter basename='/portfolio'>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 )
